b1d04c45b99ade671ebef6446f38901b4459dd72
Juliano Jose Simao <juliano.simao@educacao.sp.gov.br>
2025-11-19 16:26:07 -0300

    a

---
+++ public/projetos/imagem.js
@@ -0,0 +1,55 @@
+// IMPORTAR TODAS AS IMAGENS AQUI
+import Abalosismico3A from "./Abalosismico3A.png";
+import JogoTetris1C from "./JogoTetris1C.png";
+import LabirintoEletrico1A from "./LabirintoEletrico1A.png";
+import LampadaDeLava1C from "./LampadaDeLava1C.png";
+import LampadaDeLava2C from "./LampadaDeLava2C.png";
+import LançadorDeFoguete3A from "./LancadorDeFoguete3A.png";
+import MaleficiosDoCigarro2DS from "./MaleficiosDoCigarro2DS.png";
+import MedidorDeTemperatura30DS from "./MedidorDeTemperatura30DS.png";
+import PastaDeElefante1B from "./PastaDeElefante1B.png";
+import PianoDeBanana3ADM from "./PianoDeBanana3ADM.png";
+import PonteDeMacarrao1B from "./PonteDeMacarrao1B.png";
+import RecacaoDeCombustao1C from "./RecacaoDeCombustao1C.png";
+import RoboHidraulico1A from "./RoboHidraulico1A.png";
+import RobodeArduinobi2DS from "./RobodeArduinobi2DS.png";
+import RoletaMatematica2C from "./RoletaMatematica2C.png";
+import SangueDoDiabo3B from "./SangueDoDiabo3B.png";
+import SerpenteDeFarao3B from "./SerpenteDeFarao3B.png";
+import Simografo3B from "./Simografo3B.png";
+import SistemaCardioCirculatorio1B from "./SistemaCardioCirculatorio1B.png";
+import SolidoDePlatão3B from "./SolidoDePlatao3B.png";
+import TabuleiroEletrico1A from "./TabuleiroEletrico1A.png";
+import TornadoDeFogo2A from "./TornadoDeFogo2A.png";
+import TornadoAuroraBoreal3ADM from "./TornadoAuroraBoreal3ADM.png";
+import Tsunami3A from "./Tsunami3A.png";
+import TurbinaEolica2DS from "./TurbinaEolica2DS.png";
+
+// EXPORTAÇÃO AUTOMÁTICA POR NOME
+export default {
+  Abalosismico3A,
+  JogoTetris1C,
+  LabirintoEletrico1A,
+  LampadaDeLava1C,
+  LampadaDeLava2C,
+  LançadorDeFoguete3A,
+  MaleficiosDoCigarro2DS,
+  MedidorDeTemperatura30DS,
+  PastaDeElefante1B,
+  PianoDeBanana3ADM,
+  PonteDeMacarrao1B,
+  RecacaoDeCombustao1C,
+  RoboHidraulico1A,
+  RobodeArduinobi2DS,
+  RoletaMatematica2C,
+  SangueDoDiabo3B,
+  SerpenteDeFarao3B,
+  Simografo3B,
+  SistemaCardioCirculatorio1B,
+  SolidoDePlatão3B,
+  TabuleiroEletrico1A,
+  TornadoDeFogo2A,
+  TornadoAuroraBoreal3ADM,
+  Tsunami3A,
+  TurbinaEolica2DS,
+};
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,14 +3,15 @@ import styles from "../pages/escolha.module.css";
 import noimage from "../img/noimage.png";
 import axios from "axios";
 
+
 function createVoto(id) {
   axios.post(`http://localhost:5000/api/votos`, { id_projeto: id });
 }
 
-export default function ProjetoCard({ id, titulo, percentual, selecionado }) {
+export default function ProjetoCard({ id, titulo, percentual, selecionado}) {
   return (
     <div className={styles.card}>
-      <img src={noimage} alt="Imagem do projeto" className={styles.imagem} />
+      <img  src={`/projetos/${id}.png`} alt="Imagem do projeto" className={styles.imagem} />
       <h3>{titulo}</h3>
       <p>Percentual: {percentual}%</p>
       <button className={styles.botaoVotar} onClick={() => createVoto(id)}>
